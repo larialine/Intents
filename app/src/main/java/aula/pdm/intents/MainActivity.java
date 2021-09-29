f0be4550ea7d4f2cd606ef17393ae91083a846a6
Larissa Aline <larissaampaulino@gmail.com>
2021-09-29 07:49:38 -0300

    implementação ACTION_VIEW, ACTION_CALL, ACTION_DIAL, ACTION_PICK ACTION_CHOOSER.

--- app/src/main/java/aula/pdm/intents/MainActivity.java
+++ app/src/main/java/aula/pdm/intents/MainActivity.java
@@ -1,13 +1,24 @@
 package aula.pdm.intents;
 
+import static android.Manifest.permission.CALL_PHONE;
+import static android.content.Intent.ACTION_CHOOSER;
+import static android.content.Intent.ACTION_PICK;
+import static android.content.Intent.ACTION_VIEW;
+import static android.content.Intent.EXTRA_TITLE;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -19,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView retorno;
 
     private ActivityResultLauncher<Intent> outraActivityResultLauncher;
+    private ActivityResultLauncher<String> requisicaoPermissaoActivityResultLauncher;
+    private ActivityResultLauncher<Intent> selecionarImagemActivityResultLauncher;
+    private ActivityResultLauncher<Intent> escolherAplicativoActivityResultLauncher;
 
     public static String PARAMETRO = "PARAMETRO";
 
@@ -33,8 +47,66 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Tratando intents");
         getSupportActionBar().setSubtitle("Principais tipos");
 
+
+        outraActivityResultLauncher = registerForActivityResult(
+                 new ActivityResultContracts.StartActivityForResult(),
+                 new ActivityResultCallback<ActivityResult>() {
+
+                     public void onActivityResult(ActivityResult result){
+                         if(result.getResultCode() == Activity.RESULT_OK){
+                             result.getData().getStringExtra(PARAMETRO).contains(retorno.getText());
+                         }
+                    }
+                }
+        );
+
+
+       requisicaoPermissaoActivityResultLauncher = registerForActivityResult(
+               new ActivityResultContracts.RequestPermission(),
+               new ActivityResultCallback<Boolean>() {
+                   @Override
+                   public void onActivityResult(Boolean concedida) {
+                       if(!concedida){
+                           //requisitar permissão para o usuário
+                           requisitarPermissaoLigacao();
+                       }else{
+                           chamarTelefone();
+                       }
+                   }
+               }
+       );
+
+
+        selecionarImagemActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        visualizarImagem(result);
+                    }
+                }
+        );
+
+
+        escolherAplicativoActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        visualizarImagem(result);
+                    }
+                }
+        );
+
     }
 
+    public void visualizarImagem(ActivityResult resultado){
+        if(resultado.getResultCode() == RESULT_OK){
+            Intent visualizarImagemIntent = new Intent(ACTION_VIEW);
+            visualizarImagemIntent.setData(resultado.getData().getData());
+            startActivity(visualizarImagemIntent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,21 +132,76 @@ public class MainActivity extends AppCompatActivity {
         }
         else if(item.getItemId() == R.id.viewMi){
             //Abrir um navegador
+            String url;
+            String it = parametro.getText().toString();
+            if(!it.toLowerCase().contains("http[s]?")){
+                url = "http://"+it;
+            }else{
+                url = it;
+            }
+
+            Intent siteIntent = new Intent(ACTION_VIEW, Uri.parse(url));
+            startActivity(siteIntent);
             return true;
+
         }else if (item.getItemId() == R.id.callMi){
             //Abrir uma chamada
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    //requisitar a permissão para o usuário
+                    requisitarPermissaoLigacao();
+                }else{
+                    //chamar o telefone
+                    chamarTelefone();
+                }
+            }else{
+                //chamar o telefone direto pois, permissão foi dada durante processo de instalação.
+                chamarTelefone();
+            }
             return true;
         }else if(item.getItemId() == R.id.dialMi){
             //Abrir uma discador
+            Intent discadorIntent = new Intent(Intent.ACTION_DIAL);
+            discadorIntent.setData(Uri.parse("tel: " + parametro.getText()));
+            startActivity(discadorIntent);
+
             return true;
         }else if(item.getItemId() == R.id.pickMi){
             //Abrir uma imagem
+            selecionarImagemActivityResultLauncher.launch(prepararImagemIntent());
+
             return true;
         }
         else if(item.getItemId() == R.id.chooserMi){
             //Abrir listas de aplicativos
+            Intent escolherActivityIntent = new Intent(ACTION_CHOOSER);
+            escolherActivityIntent.putExtra(Intent.EXTRA_INTENT, prepararImagemIntent());
+            escolherActivityIntent.putExtra(EXTRA_TITLE, "Escolha um aplicativo");
+            escolherAplicativoActivityResultLauncher.launch(escolherActivityIntent);
+
             return true;
-        }else
-            return false;
+        }
+
+        return false;
     }
+
+    public Intent prepararImagemIntent(){
+        Intent pegarImagemIntent = new Intent(ACTION_PICK);
+        String diretorio = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath();
+        pegarImagemIntent.setDataAndType(Uri.parse(diretorio), "image/*");
+
+        return pegarImagemIntent;
+    }
+
+    public void requisitarPermissaoLigacao(){
+        requisicaoPermissaoActivityResultLauncher.launch(CALL_PHONE);
+    }
+
+    public void chamarTelefone(){
+        Intent chamarIntent = new Intent();
+        chamarIntent.setAction(Intent.ACTION_CALL);
+        chamarIntent.setData(Uri.parse("tel: " + parametro.getText()));
+        startActivity(chamarIntent);
+    }
+
 }
